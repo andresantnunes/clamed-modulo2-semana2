d52bca908201b4a92c4383435cc7f78258af55ce
André Nunes <andre.n@edu.sc.senai.br>
2022-10-05 20:16:49 -0300

    aulas adicionadas

---
+++ aula3/src/ForAula.java
@@ -0,0 +1,16 @@
+public class ForAula {
+    public static void main(String[] args) {
+        for(int index =0 ; index < 10; index++){
+            System.out.println(index);
+        }
+        for(int index =0 ; index < 10; index++){
+            System.out.println(index);
+        }
+        for(int index =0 ; index < 10; index++){
+            System.out.println(index);
+        }
+        for(int index =0 ; index < 10; index++){
+            System.out.println(index);
+        }
+    }
+}
